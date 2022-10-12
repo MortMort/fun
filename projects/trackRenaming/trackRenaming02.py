728974a567a80640f27e57201dc2c909c1df41b7
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-10-12 07:53:50 +0200

    first lower -> upper case implementation works. It's shit but it works

--- projects/trackRenaming/trackRenaming02.py
+++ projects/trackRenaming/trackRenaming02.py
@@ -13,15 +13,17 @@ import sys                  # Exit code when error occurs
 
 
 # EDIT: Define patterns to look for and the replacements:
-patterns = ["^(\d{2}. |\w\d\. |\w\. )(.+)( - )", 
-            "^(\d{2}|\w\d|\w)( )(.+)( - )", 
-            "^(- |\. )",
-            "  {2,}"
+patterns = ["^(\d{2}. |\w\d\. |\w\. )(.+)( - )",    # 01. -> 
+            "^(\d{2}|\w\d|\w)( )(.+)( - )",         # 01 ->
+            "^(- |\. )",                            # Remove "- " and ". "
+            "  {2,}",                               # Remove "  "
+            r"\b([a-z])\B"                            # Uppercase
             ]
 replacements = [r'\2\3\1', 
                 r'\3\4\1. ', 
                 r'',
-                r''
+                r'',
+                r'\1'
                 ]
 
 
@@ -60,13 +62,29 @@ for dir in all_dirs:
                 if not match :
                     continue
                 try:
-                    # Replace the match pattern with the replacement
-                    new_name = re.sub(patterns[ii], replacements[ii], filename)
-                    new_name_full = os.path.join(dir, new_name)
+                    if ii == 4:
+                        lower_case = re.findall(patterns[ii], filename)
+                        print(lower_case)
+                        nn = 0
+                        upper_case = []
+                        new_name_temp = filename
+                        for char in lower_case:
+                            char_upper = char.upper()
+                            upper_case.append(char_upper)
+                            char_pattern = r"\b(" + char + ")\B"
+                            new_name_temp = re.sub(char_pattern, char_upper, new_name_temp)
+                            nn = nn+1
+                        print(new_name_temp)
+                        new_name = new_name_temp
+                        new_name_full = os.path.join(dir, new_name)
+                    else: 
+                        # Replace the match pattern with the replacement
+                        new_name = re.sub(patterns[ii], replacements[ii], filename)
+                        new_name_full = os.path.join(dir, new_name)
                 except re.error:
                     continue
-                if os.path.isfile(new_name_full):
-                    print('%s -> %s SKIPPED' % (filename, new_name))
+                if (os.path.isfile(new_name_full) and ii != 4):
+                    os.rename(full_path, new_name_full)
                 else:
                     os.rename(full_path, new_name_full)
                     print('%s -> %s SUCCESS' % (filename, new_name))
