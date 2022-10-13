a4b52f5c209b9bceaf8cc160be9c16a1df86d8a4
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-10-13 22:35:37 +0200

    Semms to be working like it should now with giving it a list. It runs through the name several times until all pattern matches run out

--- projects/trackRenaming/trackRenaming02.py
+++ projects/trackRenaming/trackRenaming02.py
@@ -11,87 +11,186 @@ import sys                  # Exit code when error occurs
 # TO-DO: Remove any double spaces
 
 
+# FUNCTIONS
+def select_dir():
+    root = tk.Tk()
+    root.withdraw()
+
+    # Opens a pop-up. 
+    selected_dir = filedialog.askdirectory()
+
+    # Check whether a folder was selected. Exit code if not
+    if len(selected_dir) == 0:
+        print("No directory selected!")
+    else:
+        return selected_dir
+
+
+def handle_subdirs(directory, incl_subdirs):
+    # A simple function for
+    if incl_subdirs:
+        # Add /**/ to path such that subdirectories are found when using glob()
+        path = os.path.join(directory, "**", "")
+        return glob(path, recursive=True)
+    else:
+        path = os.path.join(directory, "")
+        return [path]
+        
+    
+    # Create a list of paths
+    #return glob(path, recursive=True)
+
+
+def pattern_replace(directory, patterns, replacements, incl_subdirs):
+
+    all_dirs = handle_subdirs(directory, incl_subdirs)
+
+    print("List of renamed files:")
+
+    for ii in range(len(patterns)):
+        # Loop through the list of directories
+        for dir in all_dirs:
+            # Create a list of the files inside the directory
+            files = os.listdir(dir)
+                
+            # Loop through every file_name in the given directory
+            for file_name in files:
+                # Combine path and file_name
+                full_file_path = os.path.join(dir, file_name)
+                # Check if an actual file is located at the full_file_path location
+                if os.path.isfile(full_file_path):
+                    # Create a match object. If no match is found None is returned
+                    match = re.search(patterns[ii], file_name)
+                    if not match :
+                        continue
+                    try:
+                        new_name = re.sub(patterns[ii], replacements[ii], file_name)
+                        new_name_full = os.path.join(dir, new_name)
+                    except re.error:
+                        continue
+                    os.rename(full_file_path, new_name_full)
+                    print('%s -> %s' % (file_name, new_name))
+
+
+
+def upper_case_first_letter(directory, incl_subdirs):
+
+    # Creates a list of subdirectories. If incl_subdirs if false
+    # the list just includes one directory
+    all_dirs = handle_subdirs(directory, incl_subdirs)
+
+    # The following pattern will pick out any lowercase characters at beginning
+    # of words
+    pattern = r"(?<!\.)\b[a-z]"
+
+    print("List of uppercase'd files:")
+
+    for dir in all_dirs:
+        # Create a list of the files inside the directory
+        files = os.listdir(dir)
+        # Loop through every file_name in the given directory
+        for file_name in files:
+            # Combine path and file_name
+            full_file_path = os.path.join(dir, file_name)
+            # Check if an actual file is located at the full_file_path location
+            if os.path.isfile(full_file_path):
+                # Create a match object. If no match is found None is returned
+                match = re.search(pattern, file_name)
+                if not match :
+                    continue
+                try:
+                    # Find all (lowercase) letters matching the pattern
+                    lower_case = re.findall(pattern, file_name)
+                    new_name_temp = file_name # Predefine temporary name from file_name
+                    # In the following loop lowercase character is flipped to
+                    # upper case and replaced with the re.sub() function iteratively
+                    for char in lower_case:
+                        char_pattern = r"(?<!\.)\b" + char  # NOTE: This pattern should match
+                                                            # the pattern in the top of the script
+                        new_name_temp = re.sub(char_pattern, char.upper(), new_name_temp)
+                    new_name = new_name_temp    
+                    new_name_full = os.path.join(dir, new_name)
+                except re.error:
+                    continue
+                os.rename(full_file_path, new_name_full)
+                print('%s -> %s SUCCESS' % (file_name, new_name))
+
+
+def create_filelist(all_dirs):
+
+    # IF all_dirs is not in a list format then make it into a list
+    if str(type(all_dirs)) == "<class 'str'>":
+        dir_list = [all_dirs]
+    else:
+        dir_list = all_dirs
+
+    # Predefine list of paths to every file
+    full_file_path_list = []
+    file_list = []
+    for dir in dir_list:
+        # Create a list of the files inside the directory
+        files = os.listdir(dir)
+
+        # Loop through every file_name in the given directory
+        for file_name in files:
+            file_list.append(file_name)
+            # Combine path and file_name
+            full_file_path = os.path.join(dir, file_name)
+            # Check if an actual file is located at the full_file_path location
+            if os.path.isfile(full_file_path):
+                full_file_path_list.append(full_file_path)
+    
+    return file_list, full_file_path_list
+
+
+# ==============================================================================
+# SCRIPT
 
 # EDIT: Define patterns to look for and the replacements:
-patterns = ["^(\d{2}. |\w\d\. |\w\. )(.+)( - )",    # 01. -> 
+
+# Patterns for moving ex 01 and A1 to after " - "
+patterns = [
+            "^(\d{2}. |\w\d\. |\w\. )(.+)( - )",    # 01. -> 
             "^(\d{2}|\w\d|\w)( )(.+)( - )",         # 01 ->
             "^(- |\. )",                            # Remove "- " and ". "
-            "  {2,}",                               # Remove "  "
-            r"(?<!\.)\b[a-z]"                      # Uppercase
+            "  {2,}"                                # Remove two ore more spaces
             ]
-replacements = [r'\2\3\1', 
+replacements = [
+                r'\2\3\1', 
                 r'\3\4\1. ', 
                 r'',
-                r'',
-                r''         # This row is irrelevant since special replacement is 
-                            # run at the 4th pattern (counting from 0)
+                r''
                 ]
 
-
-# Open pop-up to input folder for file renaming
-# ==============================================================================
-root = tk.Tk()
-root.withdraw()
-
-# Opens a pop-up. The selected directory path goes into selected_dir
-selected_dir = filedialog.askdirectory()
+# Patterns for replacing ex 01 with A1.
+patterns2 = [
+            r"((01 )|(01\. ))",
+            r"((02 )|(02\. ))",
+            r"((03 )|(03\. ))",
+            r"((04 )|(04\. ))",
+            r"( \w\d)( )"
+            ]
+replacements2 = [
+                r'A1. ',
+                r'A2. ',
+                r'B1. ',
+                r'B2. ',
+                r'\1.\2'
+                ]
 
 # Adding this line to more easily differnetiate file erros and script output
 # form the annoying Alpha value message..
 print("------------------------------------------------------------------")
 
-# Check whether a folder was selected. Exit code if not
-if len(selected_dir) == 0:
-    print("Error! No directory selected. Stopping the script")
-    sys.exit()
+# Opens a pop-up. The selected directory path goes into selected_dir
+selected_dir = select_dir()
 
-# Add /**/ to path such that subdirectories are found when using glob()
-path = os.path.join(selected_dir, "**", "")
+#pattern_replace(selected_dir, patterns, replacements, incl_subdirs = True)
+#upper_case_first_letter(selected_dir, incl_subdirs=True)
 
-# Create list of folders and subfolders
-all_dirs = glob(path, recursive=True)
-# Loop through the list of directories
-for dir in all_dirs:
-    # Loop through every pattern
-    for ii in range(len(patterns)):
-        # Create a list of the files inside the directory
-        files = os.listdir(dir)
-        # Loop through every filename in the given directory
-        for filename in files:
-            # Combine path and filename
-            full_path = os.path.join(dir, filename)
-            # Check if an actual file is located at the full_path location
-            if os.path.isfile(full_path):
-                # Create a match object. If no match is found None is returned
-                match = re.search(patterns[ii], filename)
-                if not match :
-                    continue
-                try:
-                    # The 4th pattern has special code assigned to it:
-                    if ii == 4:
-                        # Find all (lowercase) letters matching the pattern
-                        lower_case = re.findall(patterns[ii], filename)
-                        new_name_temp = filename # Predefine temporary name from filename
-                        nn = 0
-                        # In the following loop lowercase character is flipped to
-                        # upper case and replaced with the re.sub() function iteratively
-                        for char in lower_case:
-                            char_upper = char.upper()       # Set upper case
-                            char_pattern = r"(?<!\.)\b" + char # NOTE: This pattern should match
-                                                               # the pattern in the top of the script
-                            new_name_temp = re.sub(char_pattern, char_upper, new_name_temp)
-                            nn = nn+1
-                        print(new_name_temp)
-                        new_name = new_name_temp
-                        new_name_full = os.path.join(dir, new_name)
-                    else: 
-                        # Replace the match pattern with the replacement
-                        new_name = re.sub(patterns[ii], replacements[ii], filename)
-                        new_name_full = os.path.join(dir, new_name)
-                except re.error:
-                    continue
-                if (os.path.isfile(new_name_full) and ii != 4):
-                    os.rename(full_path, new_name_full)
-                else:
-                    os.rename(full_path, new_name_full)
-                    print('%s -> %s SUCCESS' % (filename, new_name))
+# PROBLEM: the function does not run through pattern recursively!
+#for ii in range(len(patterns2)):
+pattern_replace(selected_dir, patterns2, replacements2, incl_subdirs=False)
+
+
+#file_list, full_file_path_list = create_filelist(selected_dir)
